fc6c24badb52715243fdabff7d6278635be8d019
TSIANOY Karl Elvestino Dorelin <elvestinodorelin@gmail.com>
2024-06-11 16:30:38 +0300

    style : finish code

--- src/main/java/com/bibliotheque/gestionbibliotheque/Config/WebConfig.java
+++ src/main/java/com/bibliotheque/gestionbibliotheque/Config/WebConfig.java
@@ -18,21 +18,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 // @EnableWebSecurity
 public class WebConfig {
 
-    // @Bean
-    // public WebMvcConfigurer corsConfig() {
-    // return new WebMvcConfigurer() {
-    // @Override
-    // public void addCorsMappings(CorsRegistry registry) {
-    // registry.addMapping("*").allowedOrigins("http://localhost:4200")
-    // .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(),
-    // HttpMethod.DELETE.name())
-    // .allowedHeaders(HttpHeaders.CONTENT_TYPE, HttpHeaders.AUTHORIZATION);
-
-    // }
-    // };
-
-    // }
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
